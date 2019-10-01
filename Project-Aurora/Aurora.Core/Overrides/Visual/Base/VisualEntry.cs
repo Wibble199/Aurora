82017e4ebf7156999c3ca48731d93f76fdbf446b
Will Bennion <will.bennion@outlook.com>
2019-10-01 22:34:38 +0100

    Implemented passing of params to override entries.

--- Project-Aurora/Aurora.Core/Overrides/Visual/Base/VisualEntry.cs
+++ Project-Aurora/Aurora.Core/Overrides/Visual/Base/VisualEntry.cs
@@ -11,6 +11,7 @@ namespace Aurora.Core.Overrides.Visual.Base {
     public class VisualEntry : VisualAtomic {
 
         public IEnumerable<(string name, Type type)> Parameters { get; }
+        public Dictionary<string, string> ParameterMap { get; set; } = new Dictionary<string, string>();
         public VisualBody Body { get; set; } = new VisualBody();
 
         public VisualEntry(IEnumerable<(string name, Type type)> parameters = null) {
@@ -21,9 +22,15 @@ namespace Aurora.Core.Overrides.Visual.Base {
         /// Creates a <see cref="LambdaExpression"/> that represents the expression tree to be invoked by
         /// this entry, including defining the parameters to be passed to the entry.
         /// </summary>
-        public LambdaExpression GetLambda(VisualProgram context) => Expression.Lambda(
-            Body.GetStatement(context),
-            Parameters.Select(p => Expression.Parameter(p.type, p.name))
-        );
+        public LambdaExpression GetLambda(VisualProgram context) {
+            var @params = Parameters.Select(p => Expression.Parameter(p.type, p.name)).ToList();
+            return Expression.Lambda(
+                Expression.Block(
+                    ParameterMap.Select((kvp, i) => VariableSetter.GetStatement(context, kvp.Value, @params[i])).Concat( // Copy any required parameters into their specified parameters
+                    Body.Statements.Select(s => s.GetStatement(context))) // Run the main code body
+                ),
+                @params
+            );
+        }
     }
 }
--- Project-Aurora/Aurora.Core/Overrides/Visual/VariableSetter.cs
+++ Project-Aurora/Aurora.Core/Overrides/Visual/VariableSetter.cs
@@ -1,25 +1,28 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq.Expressions;
 using System.Reflection;
 using Aurora.Core.Overrides.Visual.Base;
 
 namespace Aurora.Core.Overrides.Visual {
-    public class VariableSetter<T> : VisualStatement {
-
-        private static MethodInfo dictionarySetter = typeof(Dictionary<string, object>).GetMethod("set_Item");
+    public class VariableSetter<T> : VisualStatement {        
 
         public string VariableName { get; set; }
         public VisualExpression<T> Value { get; set; }
 
-        public override Expression GetStatement(VisualProgram context) =>
+        public override Expression GetStatement(VisualProgram context) =>VariableSetter.GetStatement(context, VariableName, Value.GetExpression(context));
+    }
+
+    internal static class VariableSetter {
+
+        private static MethodInfo dictionarySetter = typeof(Dictionary<string, object>).GetMethod("set_Item");
+
+        internal static Expression GetStatement(VisualProgram context, string variableName, Expression value) =>
             Expression.Call(
                 Expression.Constant(context.VariableValues), // The instance on which the method will be called (in this case, the variable dictionary)
                 dictionarySetter, // The method that will be called on the instance (the dictionary's set_Item method which is what dict[x] = y uses)
-                Expression.Constant(VariableName), // The name of the custom variable in the dictionary
-                Expression.Convert(
-                    Value.GetExpression(context), // We need to manually convert the value to an object to be able to set it in the dictionary
-                    typeof(object)
-                )
+                Expression.Constant(variableName), // The name of the custom variable in the dictionary
+                Expression.Convert(value, typeof(object)) // We need to manually convert the value to an object to be able to set it in the dictionary
             );
     }
 }
